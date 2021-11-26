025a671160e1bb04d71da91f09edc7d68172f55e
DanielBrahano <daniel431994@gmail.com>
2021-11-26 19:30:05 +0200

    little edit

--- Viewer/src/Camera.cpp
+++ Viewer/src/Camera.cpp
@@ -45,16 +45,6 @@ glm::mat4x4 Camera::GetViewTransformation()
 	up = glm::vec3(0.0f, 1.0f, 0.0f);
 	glm::mat4 test= glm::lookAt(eye, at, up);
 
-	/*for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++)
-		{
-			std::cout << test[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}*/
-	//std::cout << std::endl;
-	//std::cout << std::endl;
-	//std::cout << std::endl;
 	return glm::lookAt(eye, at, up);
 }
 
